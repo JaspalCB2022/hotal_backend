5802873ab1fd3042512091e271be70cefb84da3f
Ankit Kumar <ankit@creativebuffer.com>
2023-11-20 14:01:02 +0530

    customer, order,order_item table

--- hotelapp/hotelapp/settings.py
+++ hotelapp/hotelapp/settings.py
@@ -50,11 +50,13 @@ INSTALLED_APPS = [
     # local app module
     "account",
     "restaurant",
+    "order_management",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -182,8 +184,6 @@ SPECTACULAR_SETTINGS = {
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
     "http://localhost:3000",
     "http://127.0.0.1:9000",
 ]
