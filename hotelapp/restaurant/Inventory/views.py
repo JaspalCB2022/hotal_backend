3c3934b70f7b5befc37dc34bdd18ccd7b859d808
JaspalCB2022 <jaspal@creativebuffer.com>
2023-11-24 15:23:53 +0530

    ok

--- hotelapp/restaurant/Inventory/views.py
+++ hotelapp/restaurant/Inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.http import Http404
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -53,57 +54,99 @@ class InventoryListApiView(APIView):
 
 class InventoryCreateApiView(APIView):
     permission_classes = [permissions.IsAuthenticated, IsRestaurant]
+    parser_classes = (MultiPartParser, FormParser)
 
-    def post(self, request):
-        menu_type = request.data.get("menu_type")
-        menu_subtype = request.data.get("menu_subtype")
-        unit_category = request.data.get("unit_category")
-        restaurant = request.user.restaurant
+    def post(self, request, *args, **kwargs):
+        json_data = request.data.get("json_data", {})
+        image_file = request.data.get("image")
 
-        if not menu_subtype or not menu_type or not unit_category:
-            return Response(
-                {"message": "Validation error"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        try:
-            menu_type = get_object_or_404(MenuTypes, id=menu_type)
-        except Http404:
-            return Response(
-                {"message": "menu type not found"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        try:
-            menu_subtype = get_object_or_404(Menu_Subtype, id=menu_subtype)
-        except Http404:
-            return Response(
-                {"message": "menu subtype not found"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        try:
-            unit_category = get_object_or_404(UnitCategory, id=unit_category)
-        except Http404:
-            return Response(
-                {"message": "unit not found"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        inventory_serializer = InventoryInputSerializer(data=request.data)
-        if inventory_serializer.is_valid(raise_exception=True):
+        # Validate and save JSON data
+        inventory_serializer = InventoryInputSerializer(data=json_data)
+        if inventory_serializer.is_valid():
+            menu_type = get_object_or_404(MenuTypes, id=json_data.get("menu_type"))
+            menu_subtype = get_object_or_404(Menu_Subtype, id=json_data.get("menu_subtype"))
+            unit_category = get_object_or_404(UnitCategory, id=json_data.get("unit_category"))
+
             new_inventory = inventory_serializer.save(
                 menu_type=menu_type,
                 menu_subtype=menu_subtype,
                 unit_category=unit_category,
-                restaurant=restaurant,
+                restaurant=request.user.restaurant,
             )
+
+            # Save image file if provided
+            if image_file:
+                new_inventory.image = image_file
+                new_inventory.save()
+
             inventory_output_serializer = InventoryOutputSerializer(
                 new_inventory, context={"request": request}
             )
-        response_data = {
-            "status": status.HTTP_200_OK,
-            "error": False,
-            "detail": inventory_output_serializer.data,
-            "message": "",
-        }
-        return Response(response_data, status=status.HTTP_200_OK)
+
+            response_data = {
+                "status": status.HTTP_201_CREATED,
+                "error": False,
+                "detail": [],
+                "message": "Inventory created successfully.",
+                "data": inventory_output_serializer.data,
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(
+                {"message": "Validation error", "errors": inventory_serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    
+    # def post(self, request):
+    #     menu_type = request.data.get("menu_type")
+    #     menu_subtype = request.data.get("menu_subtype")
+    #     unit_category = request.data.get("unit_category")
+    #     restaurant = request.user.restaurant
+
+    #     if not menu_subtype or not menu_type or not unit_category:
+    #         return Response(
+    #             {"message": "Validation error"},
+    #             status=status.HTTP_404_NOT_FOUND,
+    #         )
+    #     try:
+    #         menu_type = get_object_or_404(MenuTypes, id=menu_type)
+    #     except Http404:
+    #         return Response(
+    #             {"message": "menu type not found"},
+    #             status=status.HTTP_404_NOT_FOUND,
+    #         )
+    #     try:
+    #         menu_subtype = get_object_or_404(Menu_Subtype, id=menu_subtype)
+    #     except Http404:
+    #         return Response(
+    #             {"message": "menu subtype not found"},
+    #             status=status.HTTP_404_NOT_FOUND,
+    #         )
+    #     try:
+    #         unit_category = get_object_or_404(UnitCategory, id=unit_category)
+    #     except Http404:
+    #         return Response(
+    #             {"message": "unit not found"},
+    #             status=status.HTTP_404_NOT_FOUND,
+    #         )
+    #     inventory_serializer = InventoryInputSerializer(data=request.data)
+    #     if inventory_serializer.is_valid(raise_exception=True):
+    #         new_inventory = inventory_serializer.save(
+    #             menu_type=menu_type,
+    #             menu_subtype=menu_subtype,
+    #             unit_category=unit_category,
+    #             restaurant=restaurant,
+    #         )
+    #         inventory_output_serializer = InventoryOutputSerializer(
+    #             new_inventory, context={"request": request}
+    #         )
+    #     response_data = {
+    #         "status": status.HTTP_200_OK,
+    #         "error": False,
+    #         "detail": [],
+    #         "message": "Inventory created successfully.",
+    #     }
+    #     return Response(response_data, status=status.HTTP_200_OK)
 
 
 class InventoryDeleteApiView(APIView):
